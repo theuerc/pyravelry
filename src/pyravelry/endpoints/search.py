d119048c6f045491a78adad675232bdb6278d108
Coulton Theuer <theuerc@umich.edu>
2025-12-27 21:59:11 -0500

    fix typing

--- src/pyravelry/endpoints/search.py
+++ src/pyravelry/endpoints/search.py
@@ -3,7 +3,7 @@
 https://www.ravelry.com/api#/_search
 """
 
-from typing import Any
+from typing import Literal, Optional
 
 from pyravelry.endpoints.base import BaseEndpoint
 from pyravelry.models import GlobalSearchResponseModel, SearchParams, SearchResultModel
@@ -16,14 +16,37 @@ class SearchResource(BaseEndpoint):
     input_model = SearchParams
     output_model = GlobalSearchResponseModel
 
-    def query(self, **kwargs: Any) -> list[SearchResultModel]:
+    def query(
+        self,
+        query: str,
+        limit: int = 10,
+        types: Optional[
+            list[
+                Literal[
+                    "User",
+                    "PatternAuthor",
+                    "PatternSource",
+                    "Pattern",
+                    "YarnCompany",
+                    "Yarn",
+                    "Group",
+                    "Event",
+                    "Project",
+                    "Page",
+                    "Topic",
+                    "Shop",
+                ]
+            ]
+            | None
+        ] = None,
+    ) -> list[SearchResultModel]:
         """
         Perform a global search.
 
         Args:
             query (str): Any fulltext string
             limit (int): integer between 1 and 50
-            types (str|list): space delineated string or list of strings
+            types (str|list): Optional. space delineated string or list of strings
                 for the types of categories that you are searching.
                 See client.search.param_model.model_json_schema()
                 for available types.
@@ -31,7 +54,7 @@ class SearchResource(BaseEndpoint):
         Usage:
             search.query(query="merino", limit=10, types=["Yarn"])
         """
-        params_obj = SearchParams(**kwargs)
+        params_obj = SearchParams(query=query, limit=limit, types=types)
 
         # Flatten the 'types' list into a space-delimited string
         params_dict = params_obj.model_dump(exclude_none=True)
